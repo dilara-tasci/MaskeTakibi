a65c88580cf33451378d5edd29113ffb5a665d4f
dilara-tasci <dilara-tasci@github.com>
2021-08-16 15:51:37 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PttManager: ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService) //Constructor new yapıldığında çalışır
+        {
+            _applicantService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            // new şeklinde başka sınıf kullanmak sınıfları birbire bağımlı hale getirir ve sorun çıkarabilir.
+            // PersonManager personManager = new PersonManager();
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,46 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Vatandas vatandas = new Vatandas();
+            SelamVer();
+            SelamVer("Dilara");
+            int sonuc = Topla(3, 5);
+            Person person1 = new Person();
+            person1.FirstName = "ENGİN";
+            person1.LastName = "DEMİROĞ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 123;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+        static void SelamVer(string name = "Yeni Kullanıcı")
+        {
+            Console.WriteLine("Merhaba " + name);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam = " + sonuc);
+            return sonuc;
+        }
+    }
+
+    class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
